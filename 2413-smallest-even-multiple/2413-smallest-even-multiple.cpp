4504e1e89c4f83d3f4edd10e6097d7f0a88094e0
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-10-12 22:54:26 +0530

    Update 2413-smallest-even-multiple.cpp

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -16,7 +16,7 @@ public:
 };
 
 /*
-Approach: 
+My Approach (Implemented in the above code): 
 1] Store n in a variable 'multipleOfN'
 2] initialize i = 1. This 'i' will be used for iteration, i.e checking the 2's table
 3] loop until value of 2*i is not equal to multipleOfN
@@ -26,4 +26,24 @@ Approach:
 
 If multipleOfN becomes equal to 2*i then the loop gets exited .
 And the value is returned,which is the smallest multiple of 2 and n.
+
+
+
+Optimized Approach:
+1) If n is even, return n 
+2) Else return n*2
+
+(
+If a number is even, it means it is multiple of 2.
+If it is odd, then the common multiple can be that number*2
+)
+
+code:
+if(n%2 == 0){
+return n;
+}
+else{
+return n*2;
+}
+
 */
