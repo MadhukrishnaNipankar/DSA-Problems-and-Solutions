f7d2ac086b028d3213df5b79f45c5734121a11ad
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-10-12 23:09:04 +0530

    Update 0191-number-of-1-bits.cpp

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -12,3 +12,15 @@ public:
         return count;
     }
 };
+
+/*
+My Approach(The above solution):
+1]check if the last digit of the given number is one. i.e suppose we perform (Bitwise &) operation on the number with 1. If the result is 1,
+it means the last bit of the number is 1, else if the result is 0, then the last bit is 0.
+
+2]if we find it to be 1, increase the count of 1.
+3] do right shift operation on the number with 1, so that the last digit gets eliminated.
+4] continue the above steps until number becomes 0.
+5] return count
+
+*/
