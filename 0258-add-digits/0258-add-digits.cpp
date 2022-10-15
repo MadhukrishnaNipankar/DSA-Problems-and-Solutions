7d8e34daae047ef9c116548e614cf63b838ef499
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-10-15 21:54:55 +0530

    Time: 3 ms (67.74%), Space: 5.9 MB (71.94%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -5,9 +5,14 @@ public:
             return num;
         
         int sum = 0, temp = num;
+        //loop until temp is of more than one digit
         while(temp > 9){
             sum +=(num%10);
             num/=10; 
+            
+            /*if at some point, we get sum of all digits,then eventually, 
+              num becomes 0. so, to sum the digits of the sum variable we got                 here, we reinitialize num and temp = sum
+            */
             if(num == 0){
                 num = sum;
                 temp = sum;
@@ -17,4 +22,3 @@ public:
         return temp;
     }
 };
-
