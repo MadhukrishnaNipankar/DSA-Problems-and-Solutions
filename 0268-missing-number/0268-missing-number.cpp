b0a69a44018f1e4667f66a90255b6ec0107edd40
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-10-19 19:16:58 +0530

    Time: 36 ms (60.10%), Space: 18 MB (20.50%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -13,7 +13,20 @@ public:
             arraySum = arraySum + nums[i];
         }
         
-        
-        return sum-arraySum;
+        int missingNumber = sum - arraySum;
+        return missingNumber;
     }
 };
+
+/*
+Approach: 
+ -> suppose, our array is [3,0,1].
+    so, we know that size of array is 3.
+    so if we calculate 1+2+3 which is 6 --- (1)
+    
+    and then calculate the sum of array i.e 3+0+1 , which is 4 --- (2)
+    
+    and then subtract (2) from (1), i.e  6-4 = 2.
+    we get the missing number
+ 
+*/
