9a8efc32a6074cc76cde93dd4dcfc414be736875
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-10-21 19:27:30 +0530

    Time: 164 ms (32.44%), Space: 33.6 MB (48.73%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+    vector<int> duplicates;
+        for(int i = 0;i<nums.size();i++){
+            int currentNumber = abs(nums[i]);
+            int markingIndex = currentNumber - 1;
+
+            //negating the element
+            nums[markingIndex] = nums[markingIndex]*-1;
+            
+            //if the number is positive,after multiplying it with -1, means double negation had happened, that means it is duplicate number
+            if(nums[markingIndex] > 0){
+                duplicates.push_back(currentNumber);
+            }
+        }
+
+        return duplicates;
+    }
+};
