4a2d249262459e45bf018783f8a88bb0e573b3f1
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2022-10-25 18:21:25 +0530

    Time: 46 ms (6.17%), Space: 16.2 MB (91.53%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,3 +1,6 @@
+
+//o(n) space complexity
+/*
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
@@ -12,5 +15,31 @@ public:
         
         
         return ans;
+    }
+};
+
+*/
+
+
+//o(1) space complexity
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        
+        while(i<n){
+            nums.at(i) = nums[i] + ( n * (nums[nums[i]]%n));
+            i++;
+        }
+        
+         i = 0;
+         while(i<n){
+            nums.at(i) = nums.at(i)/n;
+            i++;
+        }
+        
+        
+        return nums;
     }
 };
