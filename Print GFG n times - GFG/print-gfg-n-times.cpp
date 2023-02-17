344578975f61c25db88a07968cda28bb12c3d3c6
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2023-02-17 16:08:28 +0530

    Added solution - LeetHub

---
+++ Print GFG n times - GFG/print-gfg-n-times.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  int i = 1;
+    void printGfg(int N) {
+        if(i>N){
+            return;
+        }
+        else{
+             cout<<"GFG ";
+             i++;
+             printGfg(N);
+        }
+      
+    }
+};
+
+
+//{ Driver Code Starts.
+/* Driver program to test printNos */
+int main() {
+    int T;
+
+    // taking testcases
+    cin >> T;
+
+    while (T--) {
+        int N;
+
+        // input N
+        cin >> N;
+        Solution ob;
+        // calling printNos() function
+        ob.printGfg(N);
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
