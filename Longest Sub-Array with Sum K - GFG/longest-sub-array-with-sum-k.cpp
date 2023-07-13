4b7a28762f6e4a5e23068ad7b9d83063263a1660
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2023-07-13 12:32:28 +0530

    Added solution - LeetHub

--- Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.cpp
@@ -6,70 +6,36 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
     public:
-    int lenOfLongSubarr(int a[],  int n, int k) 
+    int lenOfLongSubarr(int A[],  int n, int K) 
     { 
-
-    map<int, int> preSumMap;
-    int sum = 0;
-    int maxLen = 0;
-    for (int i = 0; i < n; i++) {
-        //calculate the prefix sum till index i:
-        sum += a[i];
-
-        // if the sum = k, update the maxLen:
-        if (sum == k) {
-            maxLen = max(maxLen, i + 1);
+        unordered_map<int,int>hash;
+        
+        int currentSum = 0;
+        int longestSequence = 0;
+        
+        for(int i=0;i<n;i++){
+            currentSum+=A[i];//15
+            
+            if(currentSum == K){
+                longestSequence = max(longestSequence,i+1);
+            }
+            
+            if(hash.find(currentSum-K) != hash.end()){
+                //if the sum is already present in the hashmap
+                longestSequence = max(longestSequence,i-hash[currentSum-K]);
+            }
+            
+            if (hash.find(currentSum) == hash.end()) {
+                 // if the sum is not present in the hashmap, store the sum along with their indeice
+                 hash[currentSum] = i;
+                }
+               
         }
-
-        // calculate the sum of remaining part i.e. x-k:
-        int rem = sum - k;
-
-        //Calculate the length and update maxLen:
-        if (preSumMap.find(rem) != preSumMap.end()) {
-            int len = i - preSumMap[rem];
-            maxLen = max(maxLen, len);
-        }
-
-        //Finally, update the map checking the conditions:
-        if (preSumMap.find(sum) == preSumMap.end()) {
-            preSumMap[sum] = i;
-        }
-    }
-
-    return maxLen;
-    }
-    
-        //   More Optimal Solution - for only positives
-/*
-    int left = 0, right = 0; // 2 pointers
-    int sum = a[0];
-    int maxLen = 0;
-    while (right < N) {
-        // if sum > k, reduce the subarray from left
-        // until sum becomes less or equal to k:
-        while (left <= right && sum > K) {
-            sum -= a[left];
-            left++;
-        }
-
-        // if sum = k, update the maxLen i.e. answer:
-        if (sum == K) {
-            maxLen = max(maxLen, right - left + 1);
-        }
-
-        // Move forward thw right pointer:
-        right++;
-        if (right < N) sum += a[right];
-    }
-
-    return maxLen;
+        
+        return longestSequence;
     } 
-    */
-       
+
 };
-
-       
-
 
 //{ Driver Code Starts.
 
