a192cb060271ce06c45133d05aa0b94117ed177c
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2023-08-06 01:03:45 +0530

    Added solution - LeetHub

---
+++ String Permutations - GFG/string-permutations.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    //Complete this function
+    void solve(string &s,vector<string>&ans,string &temp,vector<bool> &used){
+        if(temp.size() >= s.size()){
+            ans.push_back(temp);
+            return;
+        }
+        
+        for(int i=0;i<s.size();i++){
+            // AB
+            
+            // if this index is already taken then don't take it again
+            if(!used[i]){
+                temp+=s[i]; //AB
+                used[i] = true;
+                solve(s,ans,temp,used);
+                
+                temp.pop_back();
+                used[i] = false;
+            }
+        }  
+        
+    }
+    vector<string> permutation(string s)
+    {
+     
+       vector<string>ans;
+       string temp = "";
+       vector<bool> used(s.size(), false);
+       solve(s,ans,temp,used);
+         sort(ans.begin(),ans.end());
+       return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int T;
+	cin>>T;
+	while(T--)
+	{
+		string S;
+		cin>>S;
+		Solution ob;
+		vector<string> vec = ob.permutation(S);
+		for(string s : vec){
+		    cout<<s<<" ";
+		}
+		cout<<endl;
+	
+	}
+	return 0;
+}
+// } Driver Code Ends
