1eebedc0c0839d43e81c607b6a520f45d69aac4c
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2023-08-23 00:26:23 +0530

    Added solution - LeetHub

---
+++ Pairwise swap elements of a linked list - GFG/pairwise-swap-elements-of-a-linked-list.cpp
@@ -0,0 +1,152 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+  Pairwise swap a linked list
+  The input list will have at least one element
+  node is defined as
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+}*head;
+*/
+class Solution
+{
+    public:
+    // Node* pairWiseSwap(struct Node* head) 
+    // {
+    //   if(head->next == NULL)return head;
+       
+      
+    //   Node*PREV = new Node(0);
+    //   PREV->next = head;
+    //   Node*FIRST = head;
+    //   Node*SECOND = head->next;
+    //   Node*NEXT = SECOND->next;
+      
+    //   Node*copy = head->next;
+      
+    //   while( NEXT->next!=NULL && NEXT->next->next!=NULL ){
+    //          PREV->next = SECOND;
+    //          SECOND->next = FIRST;
+    //          FIRST->next = NEXT;
+             
+    //          swap(FIRST,SECOND);
+    
+    //           PREV=PREV->next->next;
+    //           FIRST=FIRST->next->next;
+    //           SECOND = SECOND->next->next;
+    //           NEXT=NEXT->next->next;
+    //   }
+    
+  
+          
+    //   return copy;
+      
+      
+    // }
+    Node* pairWiseSwap(struct Node* head) {
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    Node* newHead = head->next;  // Store the new head after swapping
+    Node* prev = NULL;
+
+    while (head != NULL && head->next != NULL) {
+        Node* first = head;
+        Node* second = head->next;
+        Node* nextPair = second->next;
+
+        // Swap the pairs
+        if (prev != NULL) {
+            prev->next = second;
+        }
+        second->next = first;
+        first->next = nextPair;
+
+        prev = first;  // Update prev to point to the current first node
+        head = nextPair;  // Move head to the next pair's starting node
+    }
+
+    return newHead;
+}
+
+};
+
+//{ Driver Code Starts.
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		map<Node*, int> mp;
+		mp[head] = head->data;
+		for (int i = 0; i<n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+			mp[tail] = tail->data;
+		}
+		struct Node *failure = new Node(-1);
+		Solution ob;
+		head = ob.pairWiseSwap(head);
+		int flag = 0;
+		struct Node *temp = head;
+		while(temp){
+		    if(mp[temp] != temp->data){
+		        flag = 1;
+		        break;
+		    }
+		    temp = temp->next;
+		}
+		if(flag)
+		    printList(failure);
+		else
+		    printList(head);
+	}
+	return 0; 
+}
+
+// } Driver Code Ends
