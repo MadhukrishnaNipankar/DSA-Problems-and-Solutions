d9b5f636a5baf49d094015a8e5bfe10dd89b37cf
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2023-10-25 01:19:36 +0530

    Added solution - LeetHub

--- Swap two numbers - GFG/swap-two-numbers.cpp
+++ Swap two numbers - GFG/swap-two-numbers.cpp
@@ -9,6 +9,31 @@ using namespace std;
 
 class Solution{   
 public:
+/*
+    Before doing this, we should know 
+    1. n1 xor 0 = n1
+    2. n1 xor n1 = 0
+    3. ordering in xor operations doesn't matter
+    
+    Here is the logic:
+    
+    1. a = a xor b 
+    now a has a as well as b.
+    
+    2. b = a xor b
+    we know that a already has (a and b). so it a xor b will be like (
+        a xor b xor b
+    )
+    Here b xor b will cancel each other.
+    hence b will only have a.
+    
+    3. a = a xor b
+    the above line is equivalent to this:-
+    a = (a xor b xor a)
+    Here a will cancel each other
+    Hence a will only have b.
+
+*/
     pair<int, int> get(int a, int b){
         pair<int,int>ans;
         a = a^b;
