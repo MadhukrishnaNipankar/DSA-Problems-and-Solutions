8abc21e7f715a4c99e65545bd2e571a175f8efa2
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2024-06-18 12:14:49 +0530

    Time: 15 ms (50.46%), Space: 15.1 MB (9.51%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -12,34 +12,49 @@ public:
    //  }
     
     //better 2 pointer approach  O(nlogn + n)     
-vector<int> twoSum(vector<int>& nums, int target) {
-    int n = nums.size();
-    vector<pair<int, int>> numIndexPairs;
+//  vector<int> twoSum(vector<int>& nums, int target) {
+//     int n = nums.size();
+//     vector<pair<int, int>> numIndexPairs;
 
-    // Create a vector of pairs (value, index)
-    for (int i = 0; i < n; i++) {
-        numIndexPairs.push_back({nums[i], i});
-    }
+//     // Create a vector of pairs (value, index)
+//     for (int i = 0; i < n; i++) {
+//         numIndexPairs.push_back({nums[i], i});
+//     }
 
-    // Sort the pairs based on the values
-    sort(numIndexPairs.begin(), numIndexPairs.end());
+//     // Sort the pairs based on the values
+//     sort(numIndexPairs.begin(), numIndexPairs.end());
 
-    int i = 0, j = n - 1;
-    while (i < j) {
-        int sum = numIndexPairs[i].first + numIndexPairs[j].first;
-        if (sum == target) {
-            // Return a vector of the original indices
-            return {numIndexPairs[i].second, numIndexPairs[j].second};
-        } else if (sum > target) {
-            j--;
-        } else {
-            i++;
-        }
-    }
+//     int i = 0, j = n - 1;
+//     while (i < j) {
+//         int sum = numIndexPairs[i].first + numIndexPairs[j].first;
+//         if (sum == target) {
+//             // Return a vector of the original indices
+//             return {numIndexPairs[i].second, numIndexPairs[j].second};
+//         } else if (sum > target) {
+//             j--;
+//         } else {
+//             i++;
+//         }
+//     }
 
-    // Return an empty vector if no solution is found
-    return {};
-}
+//     // Return an empty vector if no solution is found
+//     return {};
+// }
     
+    
+    // optimal O(n)using hashmap .O(n) one pass solution
+    vector<int> twoSum(vector<int>& nums, int target) {
+            int n = nums.size();
+            unordered_map<int,int>hash;
+            for(int i=0;i<n;i++){
+                   if(hash[target-nums[i]])return {i,hash[target-nums[i]]-1};
+                   
+                   hash[nums[i]] = i+1; 
+            }
+        
+        return {};
+
+    }
+   
 
 };
