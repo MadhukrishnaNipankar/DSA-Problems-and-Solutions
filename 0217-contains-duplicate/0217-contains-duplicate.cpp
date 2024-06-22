12cc5584c96592724296e266d98068d489b9dbe7
Madhukrishna Nipankar <88615405+MadhukrishnaNipankar@users.noreply.github.com>
2024-06-22 12:18:45 +0530

    Time: 133 ms (10.95%), Space: 75.9 MB (6.30%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int,int>hash;
+//     using hashmap
+    // bool containsDuplicate(vector<int>& nums) {
+    //     unordered_map<int,int>hash;
+    //     int n = nums.size();
+    //     for(int i=0;i<n;i++){
+    //         hash[nums[i]]++;
+    //         if(hash[nums[i]] == 2)return true;
+    //     }
+    //     return false;
+    // }
+    
+//     using set
+     bool containsDuplicate(vector<int>& nums) {
+        set<int>s;
         int n = nums.size();
         for(int i=0;i<n;i++){
-            hash[nums[i]]++;
-            if(hash[nums[i]] == 2)return true;
+            s.insert(nums[i]);
         }
+        if(s.size()<n)return true;
         return false;
     }
 };
